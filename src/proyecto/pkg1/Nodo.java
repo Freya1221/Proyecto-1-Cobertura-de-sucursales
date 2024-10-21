91cf4af92cf3c7beacb0ebc101980b646909cb8d
RUKAYA <na.rukaya@correo.unimet.edu.ve>
2024-10-21 17:28:45 -0400

    Clase Nodo

---
+++ src/proyecto/pkg1/Nodo.java
@@ -0,0 +1,27 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package proyecto.pkg1;
+
+/**
+ *
+ * @author andre
+ */
+public class Nodo {
+    String nombre;
+    Nodo siguiente;
+    boolean es_sucursal;
+    Lista paradas;
+
+    public Nodo(String nombre) {
+        this.nombre = nombre;
+        this.siguiente = null;
+        this.es_sucursal = false;
+        this.paradas = new Lista();
+    }
+    
+    
+    
+    
+}
