abc2bb1dcb8d26ebf46ef73feeb1d0aff4b5a369
Freya <freyablanca10013@gmail.com>
2024-10-24 01:10:31 -0400

    Clase Cola

--- src/proyecto/pkg1/Cola.java
+++ src/proyecto/pkg1/Cola.java
@@ -9,5 +9,29 @@ package proyecto.pkg1;
  * @author Freya
  */
 public class Cola {
+    Nodo cabeza;
+    Nodo cola;
     
+    public Cola(){
+        this.cabeza = this.cola = null;
+    }
+    
+    public void encolar(String estacion){
+        if(this.cabeza == null){
+            this.cabeza = this.cola = new Nodo(estacion);
+        }else{
+            this.cola.siguiente = new Nodo(estacion);
+            this.cola = this.cola.siguiente;
+        }
+    }
+    
+    public Nodo desencolar(String estacion){
+        if(this.cabeza == null){
+            return null;
+        }else{
+            Nodo aux = this.cabeza;
+            this.cabeza = this.cabeza.siguiente;
+            return aux;
+        }
+    }
 }
